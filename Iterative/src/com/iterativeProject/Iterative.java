0cd306807267f50cbe82f092e695d97c6ab801fa
ragoolaman <ragoolaman@users.noreply.github.com>
2017-09-19 09:43:20 -0600

    Update Iterative.java

--- Iterative/src/com/iterativeProject/Iterative.java
+++ Iterative/src/com/iterativeProject/Iterative.java
@@ -1,15 +1,18 @@
-package com.iterativeProject;
+package com.iterativeWork;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -22,45 +25,66 @@ import javafx.stage.Stage;
 
 public class Iterative extends Application{
 
-	private Tab linearSearchTab,mergeTab;
+	//Variable declarations
+	private Tab linearSearchTab,mergeTab,sortTab;
 	private Label in,out;
-	private Button start,startMerge;
-	private TextField input,key,intArray1,intArray2;
-	private TextArea output,outputMerge;
-	private String result;
+	private Button start,startMerge,startSort;
+	private TextField input,key,intArray1,intArray2,toBeSorted;
+	private TextArea output,outputMerge,outputSort;
+	private ComboBox sortOptionSelect;
+	private String result,sortType;
 	private List<Integer> inputA = new ArrayList<Integer>();
 	private List<Integer> inputB = new ArrayList<Integer>();
+	private ObservableList<String> sortOptions = FXCollections.observableArrayList("Bubble sort","Insertion sort");
 	
 	private static Search searcher;
 	private static Merge merger;
+	private static Sort sorter;
 	/**
 	 * @param args
 	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Initialize objects for my Search, Merge, and Sort classes
 		searcher = new Search();
 		merger = new Merge();
+		sorter = new Sort();
+		//Launch the JavaFX Runtime Thread
 		launch(args);
 	}
+	@SuppressWarnings("unchecked")
 	@Override
 	public void start(Stage theStage) throws Exception {
 		// TODO Auto-generated method stub
+		//Set Frame title, and tab view
 		theStage.setTitle("Search");
 		TabPane tabs = new TabPane();
 		Scene theScene = new Scene(tabs,500,500);
+		
+		//Create tabs for Searching, Merging, and Sorting data I/O
 		linearSearchTab = new Tab();
-		linearSearchTab.setText("Linear Search");
+		linearSearchTab.setText("Search");
 		linearSearchTab.setClosable(false);
 		linearSearchTab.setContent(linearSearchPane());
+		
 		mergeTab = new Tab();
 		mergeTab.setText("Merge");
 		mergeTab.setClosable(false);
 		mergeTab.setContent(mergePane());
-		tabs.getTabs().addAll(linearSearchTab,mergeTab);
+		
+		sortTab = new Tab();
+		sortTab.setText("Sort");
+		sortTab.setClosable(false);
+		sortTab.setContent(sortPane());
+		
+		//Add tabs to the view
+		tabs.getTabs().addAll(linearSearchTab,mergeTab,sortTab);
 		theStage.setScene(theScene);
+		//Display the JavaFX Window
 		theStage.show();
 		
+		//Various miscellaneous ActionEvent Listeners
 		start.setOnAction(new EventHandler<ActionEvent>(){
 
 			@Override
@@ -122,7 +146,55 @@ public class Iterative extends Application{
 				merger.flushAll();
 			}
 		});
+		toBeSorted.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String[] stringArray = toBeSorted.getText().split(" ");
+				int[] intArray = new int[stringArray.length];
+				for(int i = 0; i < stringArray.length; i++) 
+				{
+					intArray[i] = Integer.parseInt(stringArray[i]);
+				}
+				sorter.setoutputArray(intArray);
+				outputSort.appendText("Integer array taken\n");
+			}});
+		sortOptionSelect.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+				// TODO Auto-generated method stub
+				sortType = sortOptionSelect.getValue().toString();
+				outputSort.appendText("Sort type " + sortOptionSelect.getValue().toString() + " selected\n");
+			}});
+		startSort.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(sortType == "Bubble sort") 
+				{
+					for(int i : sorter.bubbleSort()) 
+					{
+						outputSort.appendText(i + " ");
+					}
+					outputSort.appendText("\n");
+				} else if(sortType == "Insertion sort") 
+				{
+					for(int i : sorter.insertSort()) 
+					{
+						outputSort.appendText(i + " ");
+					}
+					outputSort.appendText("\n");
+					} else 
+				{
+					outputSort.appendText("Please select a sort type before running.\n");
+				}
+			}});
 	}
+	
+	//Generate content for the three tabs
 	private Pane linearSearchPane() 
 	{
 		GridPane grid = new GridPane();
@@ -180,11 +252,46 @@ public class Iterative extends Application{
 		startMerge = new Button("Begin Merge");
 
 		outputMerge = new TextArea();
+		outputMerge.setEditable(false);
 		
 		grid.add(intArray1,0,0,2,1);
 		grid.add(intArray2,0,1,2,1);
 		grid.add(startMerge,2,1);
 		grid.add(outputMerge,0,2,3,2);
+		return grid;
+	}
+	private Pane sortPane() 
+	{
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(12);
+		
+		ColumnConstraints columnLeft = new ColumnConstraints();
+		columnLeft.setHalignment(HPos.LEFT);
+		grid.getColumnConstraints().add(columnLeft);
+		
+		ColumnConstraints columnRight = new ColumnConstraints();
+		columnRight.setHalignment(HPos.RIGHT);
+		grid.getColumnConstraints().add(columnRight);
+		
+		toBeSorted = new TextField();
+		toBeSorted.setPromptText("Enter unsorted integer array delimited by space");
+		toBeSorted.setPrefWidth(200);
+		
+		sortOptionSelect = new ComboBox(sortOptions);
+		
+		startSort = new Button("Start");
+		
+		outputSort = new TextArea();
+		outputSort.setEditable(false);
+		outputSort.setPrefWidth(300);
+		
+		grid.add(toBeSorted, 0, 0,2,1);
+		grid.add(sortOptionSelect,0,1);
+		grid.add(startSort,0,2);
+		grid.add(outputSort,2,0,1,3);
+		
 		return grid;
 	}
 
